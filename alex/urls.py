4b5886457e0b19c248d58ec50b86620accbebcfd
dezalator <alex@korzh.me>
2021-08-30 20:36:44 +0300

    views and viewsets

--- alex/urls.py
+++ alex/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from login_app.views import api_accounts, RegistrUserView, UserDetailView, home
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from login_app.views import RegisterUserView, UserViewSet
+
+router = DefaultRouter()
+
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path ('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls')),
-    path('auth/', include('djoser.urls')),
-    path('api/signup/', RegistrUserView.as_view(), name='registr'),
-    path('api/accounts/', api_accounts, name='accounts'),
-    path('api/accounts/<int:pk>', UserDetailView.as_view(), name="my"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/signup/', RegisterUserView.as_view(), name='signup'),
+    path('api/v2/', include(router.urls)),
 ]
