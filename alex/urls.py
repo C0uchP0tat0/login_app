966f792292bd5c9841458a725fe0881153183f88
C0uchP0tat0 <denchik431@mail.ru>
2021-09-08 16:46:49 +0300

    update user's wallet

--- alex/urls.py
+++ alex/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from login_app.views import RegisterUserView, UserViewSet
+from wallet.views import UserWallet
 
 router = DefaultRouter()
 
@@ -15,4 +16,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/signup/', RegisterUserView.as_view(), name='signup'),
     path('api/v2/', include(router.urls)),
+    path('api/wallet/<int:pk>/', UserWallet.as_view(), name='wallet'),
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+from login_app.serializers import UserSerializer, WalletSerializer
+from .models import Wallet
+
+
+class UserWallet(APIView):
+
+    def get_object(self, pk): # получаем кошелёк пользователя по id кошелька
+        try:
+            return Wallet.objects.get(pk=pk)
+        except Wallet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user_wallet = self.get_object(pk)
+        serializer = WalletSerializer(user_wallet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None): # пополняем выбранный кошелёк
+        user_wallet = self.get_object(pk)
+        serializer = WalletSerializer(user_wallet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+   
